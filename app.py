ee7a003d391771326d51bbfb3780be7df0927e3e
Niklas <niklas.witzig@uni-mainz.de>
2022-04-29 14:58:42 +0200

    adds examples; gets rid of useless stuff (for now)

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from operator import contains
 import streamlit as st
 import numpy as np
 from pandas import DataFrame
@@ -16,6 +17,14 @@ import chess.svg
 sess = rt.InferenceSession("model/chess-resnet.onnx")
 input_name = sess.get_inputs()[0].name
 output_name = sess.get_outputs()[0].name
+
+def delete_duplicates(df):
+    df.drop_duplicates(subset=['fens'], keep='last', inplace=True)
+    return df
+
+low_examples = delete_duplicates(pd.read_csv('examples/low.csv', usecols=["fens"]))
+high_examples = delete_duplicates(pd.read_csv('examples/high.csv', usecols=["fens"]))
+medium_examples = delete_duplicates(pd.read_csv('examples/middle.csv', usecols=["fens"]))
 
 
 def fentovsg(fen):
@@ -92,7 +101,7 @@ c30, c31, c32 = st.columns([2.5, 1, 3])
 
 with c30:
     # st.image("logo.png", width=400)
-    st.title("♟️ Chess ")
+    st.title("♟️ Chess Difficulty Prediction")
     st.header("")
 
 
@@ -103,6 +112,7 @@ with st.expander("ℹ️ - About this app", expanded=True):
         """     
 -   This tool predicts the *difficulty*, for a given chess position.
 - It uses [FEN-Strings](https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation) to represent chess positions.
+- Use, e.g., the [Lichess Editor](https://lichess.org/editor) to create FEN-Strings.
 - The model is trained is a residual convolutional neural network, trained on approx. 2 mio. *professional* human chess games.
 - The Output is a single number, which yields the *difficulty* of the position, measured in Expected Loss of CP (Centipawns).
 	    """
@@ -111,92 +121,107 @@ with st.expander("ℹ️ - About this app", expanded=True):
     st.markdown("")
 
 st.markdown("")
-st.markdown("## **📌 Paste FEN-String **")
+st.markdown("## ** 🎲 Draw or 📌 Paste FEN-String **")
+fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+
+col1, col2, col3,_ = st.columns([1, 1.15, 1, 2.25])
+# get st button
+with col1:
+    if st.button("Draw Low Difficulty example"):
+        fen = low_examples.sample(n=1).iloc[0][0]
+with col2:
+    if st.button("Draw Medium Difficulty example"):
+        fen = medium_examples.sample(n=1).iloc[0][0]
+with col3:
+    if st.button("Draw High Difficulty example"):
+        fen = high_examples.sample(n=1).iloc[0][0]
 
 with st.form(key="my_form"):
 
-
-    ce, c1, ce, c2, c3 = st.columns([0.07, 1, 0.07, 5, 0.07])
-
-    with c2:
-        doc = st.text_area(
-            "Paste your FEN-String below (max 10 at once; separate multiple FEN-Strings with a newline):",
-            height=110,
+    c1,_,c3 = st.columns([5,1,5])
+    with c1:
+        doc = st.text_input(
+            "Paste your FEN-String below (max 1 at once)",
+            value=fen,
         )
-
-        MAX_WORDS = 500
-        import re
-        res = len(re.findall(r"\w+", doc))
-        if res > MAX_WORDS:
+        # check for new line
+        if doc.count('\n') > 0:
+        
             st.warning(
-                "⚠️ Your text contains "
-                + str(res)
-                + " words."
-                + " Only the first 500 words will be reviewed. Stay tuned as increased allowance is coming! 😊"
+                "⚠️ Your text contains"+
+                str(doc.count('\n')) +
+                + " FEN Strings."
+                + " Only the first will be evaluated"
             )
 
-            doc = doc[:MAX_WORDS]
+            doc = doc.split('\n')[0]
 
         submit_button = st.form_submit_button(label="✨ Predict Difficulty!")
+        
+        st.markdown(" ## Predicted Difficulty: ")
+
+        try:
+            keywords = rcnn_chess(doc)
+            if type(keywords) == list:
+                str_keywords = [str(keyword.item()) + "/n" for keyword in keywords]
+            else:
+                str_keywords = keywords.__format__('.2f')
+        except:
+            keywords = "No keywords found"
+            str_keywords = "Please provide a valid FEN-String"
+        
+        color = "White" if "w" in doc else "Black" 
+        st.markdown("**"  + str_keywords + "**" + " Expected CP Loss for " + color)
+    
+
 
     
-try:
-    keywords = rcnn_chess(doc)
-    print(keywords)
-    print(type(keywords))
-    if type(keywords) == list:
-        str_keywords = [str(keyword.item()) + "/n" for keyword in keywords]
-        print(str_keywords)
-    else:
-        str_keywords = str(keywords) + "/n"
-        print(str_keywords)
-except:
-    keywords = "No keywords found"
-    str_keywords = "Please provide a valid FEN-String"
+    with c3:
+        st.markdown('### Corresponding Board')
+        if type(doc) == list:
+            render_svg(fentovsg(doc[0]))
+        else:
+            render_svg(fentovsg(doc))
 
-st.markdown('### Board')
-if type(doc) == list:
-    render_svg(fentovsg(doc[0]))
-else:
-    render_svg(fentovsg(doc))
 
-st.markdown("## **🎈 Check & download results **")
+#st.markdown("## **🎈 Check & download results **")
 
-st.write(str_keywords)
+#st.write(str_keywords)
 
-st.header("")
+# st.header("")
 
-cs, c1, c2, c3, cLast = st.columns([2, 1.5, 1.5, 1.5, 2])
+# cs, c1, c2, c3, cLast = st.columns([2, 1.5, 1.5, 1.5, 2])
 
-with c1:
-    CSVButton2 = download_button(keywords, "Data.csv", "📥 Download (.csv)")
-with c2:
-    CSVButton2 = download_button(keywords, "Data.txt", "📥 Download (.txt)")
-with c3:
-    CSVButton2 = download_button(keywords, "Data.json", "📥 Download (.json)")
+# with c1:
+#     CSVButton2 = download_button(keywords, "Data.csv", "📥 Download (.csv)")
+# with c2:
+#     CSVButton2 = download_button(keywords, "Data.txt", "📥 Download (.txt)")
+# with c3:
+#     CSVButton2 = download_button(keywords, "Data.json", "📥 Download (.json)")
 
-st.header("")
+# st.header("")
 
-df = DataFrame({"FEN-String":[doc],"Difficulty":[keywords]}).sort_values(by="Difficulty", ascending=False)#.reset_index(drop=True)
-df.index += 1
+# df = DataFrame({"FEN-String":[doc],"Difficulty":[keywords]}).sort_values(by="Difficulty", ascending=False)#.reset_index(drop=True)
+# df.index += 1
 
-# Add styling
-cmGreen = sns.light_palette("green", as_cmap=True)
-cmRed = sns.light_palette("red", as_cmap=True)
-df = df.style.background_gradient(
-    cmap=cmGreen,
-    subset=[
-        "Difficulty",
-    ],
-)
+# # Add styling
+# cmGreen = sns.light_palette("green", as_cmap=True)
+# cmRed = sns.light_palette("red", as_cmap=True)
+# df = df.style.background_gradient(
+#     cmap=cmGreen,
+#     subset=[
+#         "Difficulty",
+#     ],
+# )
 
-c1, c2, c3 = st.columns([1, 3, 1])
+# c1, c2, c3 = st.columns([1, 3, 1])
 
-format_dictionary = {
-    "Difficulty": "{:.1%}",
-}
+# format_dictionary = {
+#     "Difficulty": "{:.1%}",
+# }
 
-df = df.format(format_dictionary)
+# df = df.format(format_dictionary)
 
 #with c2:
 #    st.table(df)
